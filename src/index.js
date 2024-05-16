e68bce2cd4cfe3b8b2d5d292b9b97c4f07bd0006
DravinanshuM <mishradravinanshu@gmail.com>
2024-05-16 16:40:41 +0530

    testing-eslint

--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@ const app = express();
 const port = 8001;
 
 app.get("/", (req, res) => {
-                                res.send("ok");
+    res.send("ok");
 })
-
-
-       console.log("hello");
 
 app.listen(port, () => {
     console.log(`app is running at http://localhost:${port}`);

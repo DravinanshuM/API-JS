22c2c373c644419d0e826f3f01f35a3bc4946698
DravinanshuM <mishradravinanshu@gmail.com>
2024-05-16 15:11:24 +0530

    create server index.js file

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# SERVER-SIDE API BUILD SOME MORE IMPORTANT PACKAGES.
+1. npm i express.
+2. npm i mongoose.
+3. npm i -D nodemon.
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 8001;
+
+app.get("/", (req, res)=>{
+    res.send("ok");
+})
+
+app.listen(port, ()=>{
+    console.log(`app is running at http://localhost:${port}`);
+})
